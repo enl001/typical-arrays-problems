399ba4c30d0af29fb537145f192515f768cffec9
enl001 <enliven001@mail.ru>
2020-02-20 18:25:05 +0300

    fix: fix 0 check

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ exports.max = array => checkIfValid(array) ? Math.max(...array) : 0;
 
 exports.avg = array => checkIfValid(array) ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 
-const checkIfValid = arr => arr != null && arr.hasElements();
+const checkIfValid = arr => (arr != null && arr.hasElements());
 
-Array.prototype.hasElements = () => !(this.length === 0);
+Array.prototype.hasElements = function(){
+    return this.length != 0;
+}
 
